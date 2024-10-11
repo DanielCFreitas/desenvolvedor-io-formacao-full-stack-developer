939a737b062ee29bdf7ecd10595928df2410f379
Daniel Corrêa <daniel.exatas@gmail.com>
2024-10-11 10:25:06 -0300

    Usando DataAnnotations para fazer validacao de formularios

---
+++ 02-fundamentos-do-asp-net-mvc/PrimeiraApp/Controllers/ModelsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class ModelsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "a",
+                Email = "daniel",
+                EmailConfirmacao = "daniel.com"
+            };
+
+
+            if (TryValidateModel(aluno))
+            {
+                return View(aluno);
+            }
+
+            var ms = ModelState;
+
+            var erros = ModelState.Select(modelState => modelState.Value.Errors)
+                .Where(error => error.Count > 0)
+                .ToList();
+
+            erros.ForEach(erro => Console.WriteLine(erro.First().ErrorMessage));
+
+            return View();
+        }
+    }
+}
