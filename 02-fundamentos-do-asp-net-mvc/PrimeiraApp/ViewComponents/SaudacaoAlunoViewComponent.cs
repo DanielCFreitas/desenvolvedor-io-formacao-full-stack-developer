8ecf89db90091c68c82252a1593ea5f29e858d64
Daniel Corrêa <daniel.exatas@gmail.com>
2024-10-12 10:13:49 -0300

    Usando ViewComponent na aplicacao

---
+++ 02-fundamentos-do-asp-net-mvc/PrimeiraApp/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.ViewComponents
+{
+    public class SaudacaoAlunoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string email)
+        {
+            // Peguei o aluno no banco de dados !!!
+            // Peguei o dado (nome) do aluno que está logado !!!
+            // Etc. . .
+            var aluno = new Aluno() { Nome = "Daniel", Email = email };
+
+            return View(aluno);
+        }
+    }
+}
