b972d8ef5bbf20bed00b3c56099581ec9c6a8814
Daniel Corrêa <daniel.exatas@gmail.com>
2024-10-13 09:11:28 -0300

    Operacoes no Banco de Dados com EF

---
+++ 02-fundamentos-do-asp-net-mvc/PrimeiraApp/Controllers/TesteEFController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Data;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class TesteEFController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public TesteEFController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            // Adicionar
+            var aluno = new Aluno()
+            {
+                Nome = "Daniel",
+                Email = "daniel.exatas@gmail.com",
+                DataNascimento = new DateTime(1992, 4, 2),
+                Avaliacao = 5,
+                Ativo = true
+            };
+
+            _appDbContext.Alunos.Add(aluno);
+            _appDbContext.SaveChanges();
+
+            // Consulta
+            var alunoChange = _appDbContext.Alunos.Where(aluno => aluno.Nome == "Daniel").FirstOrDefault();
+
+            // Atualiza
+            alunoChange.Nome = "Daniel Corrêa";
+            _appDbContext.Alunos.Update(alunoChange);
+            _appDbContext.SaveChanges();
+
+            // Exclui
+            _appDbContext.Alunos.Remove(alunoChange);
+            _appDbContext.SaveChanges();
+
+            return View();
+        }
+    }
+}
