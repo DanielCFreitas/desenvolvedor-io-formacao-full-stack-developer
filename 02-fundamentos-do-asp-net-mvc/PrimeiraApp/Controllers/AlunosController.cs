e8f17acfabd3e075638c226d2b2b31e17584f52c
Daniel Corrêa <daniel.exatas@gmail.com>
2024-10-14 08:09:25 -0300

    Criando a controller Alunos para a tela de cadastro de alunos, criado action para cadastrar

---
+++ 02-fundamentos-do-asp-net-mvc/PrimeiraApp/Controllers/AlunosController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Data;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class AlunosController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public AlunosController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao,Ativo")] Aluno aluno)
+        {
+            _appDbContext.Alunos.Add(aluno);
+            await _appDbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
