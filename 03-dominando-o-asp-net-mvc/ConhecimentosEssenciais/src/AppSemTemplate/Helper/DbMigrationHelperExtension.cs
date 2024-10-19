9468f7c86e1a9175b182c07051c4186c4770ad7a
Daniel Corrêa <daniel.exatas@gmail.com>
2024-10-19 08:56:17 -0300

    Criacao de classe para popular banco com dados ficticios

---
+++ 03-dominando-o-asp-net-mvc/ConhecimentosEssenciais/src/AppSemTemplate/Helper/DbMigrationHelperExtension.cs
@@ -0,0 +1,77 @@
+﻿using AppSemTemplate.Data;
+using AppSemTemplate.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppSemTemplate.Helper
+{
+    public static class DbMigrationHelperExtension
+    {
+        public static void UseDbMigrationHelper(this WebApplication app)
+        {
+            DbMigrationHelpers.EnsureSeedData(app).Wait();
+        }
+    }
+
+    public static class DbMigrationHelpers
+    {
+        public static async Task EnsureSeedData(WebApplication serviceScope)
+        {
+            var services = serviceScope.Services.CreateScope().ServiceProvider;
+            await EnsureSeedData(services);
+        }
+
+        public static async Task EnsureSeedData(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            var env = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            if (env.IsDevelopment() || env.IsEnvironment("Docker") || env.IsStaging())
+            {
+                await context.Database.MigrateAsync();
+                await EnsureSeedProducts(context);
+            }
+        }
+
+        private static async Task EnsureSeedProducts(AppDbContext context)
+        {
+            if (context.Produtos.Any())
+                return;
+
+            await context.Produtos.AddAsync(new Produto()
+            {
+                //Id = 1,
+                Nome = "Livro JQuery",
+                Imagem = "JQuery.jpg",
+                Valor = "50",
+            });
+
+            await context.Produtos.AddAsync(new Produto()
+            {
+                //Id = 2,
+                Nome = "Livro HTML",
+                Imagem = "HTML.jpg",
+                Valor = "100",
+            });
+
+            await context.Produtos.AddAsync(new Produto()
+            {
+                //Id = 3,
+                Nome = "Livro Razor",
+                Imagem = "Razor.jpg",
+                Valor = "150",
+            });
+
+            await context.Produtos.AddAsync(new Produto()
+            {
+                //Id = 4,
+                Nome = "Livro CSS",
+                Imagem = "CSS.jpg",
+                Valor = "200",
+            });
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- 03-dominando-o-asp-net-mvc/ConhecimentosEssenciais/src/AppSemTemplate/Program.cs
+++ 03-dominando-o-asp-net-mvc/ConhecimentosEssenciais/src/AppSemTemplate/Program.cs
@@ -1,8 +1,19 @@
+using AppSemTemplate.Data;
+using AppSemTemplate.Helper;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 var app = builder.Build();
+
+// Apenas para popular dados no banco para realizar testes
+app.UseDbMigrationHelper();
 
 // Configuracao que olha para a pasta "wwwroot" para usar os arquivos estaticos
 app.UseStaticFiles();
