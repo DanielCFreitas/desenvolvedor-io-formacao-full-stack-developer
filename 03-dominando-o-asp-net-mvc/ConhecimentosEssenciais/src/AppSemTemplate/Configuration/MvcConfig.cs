9566e5bb24385c5750aa8a8c444e0cd567517a7c
Daniel Corrêa <daniel.exatas@gmail.com>
2024-10-24 08:09:00 -0300

    Organizando a classe Program.cs

---
+++ 03-dominando-o-asp-net-mvc/ConhecimentosEssenciais/src/AppSemTemplate/Configuration/MvcConfig.cs
@@ -0,0 +1,94 @@
+﻿using AppSemTemplate.Data;
+using AppSemTemplate.Helper;
+using AppSemTemplate.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppSemTemplate.Configuration
+{
+    public static class MvcConfig
+    {
+        public static WebApplicationBuilder AddMvcConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            builder.Services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(60);
+                options.ExcludedHosts.Add("example.com");
+                options.ExcludedHosts.Add("www.example.com");
+            });
+
+            // Adicionando suporte para mudan�a de convencao de rota das areas
+            //builder.Services.Configure<RazorViewEngineOptions>(options =>
+            //{
+            //    options.AreaPageViewLocationFormats.Clear();
+            //    options.AreaPageViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+            //    options.AreaPageViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+            //    options.AreaPageViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            //});
+
+            // Forma de fazer um "transformador de rota" deixando a rota com um padrao especifico
+            //builder.Services.AddRouting(options =>
+            //    options.ConstraintMap["slugfy"] = typeof(RouteSlugfyParameterTransformer));
+
+
+            return builder;
+        }
+
+        public static WebApplication UseMvcConfiguration(this WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+                app.UseHsts();
+
+            // Redirecionamento para HTTPS
+            app.UseHttpsRedirection();
+
+            // Apenas para popular dados no banco para realizar testes
+            app.UseDbMigrationHelper();
+
+            // Configuracao que olha para a pasta "wwwroot" para usar os arquivos estaticos
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthorization();
+
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller:slugfy=Home}/{action:slugfy=Index}/{id?}");
+
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapRazorPages();
+
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                var singService = services.GetRequiredService<IOperacaoSingleton>();
+
+                Console.WriteLine("Direto do Program.cs: " + singService.OperacaoId);
+            }
+
+            return app;
+        }
+    }
+}
--- 03-dominando-o-asp-net-mvc/ConhecimentosEssenciais/src/AppSemTemplate/Program.cs
+++ 03-dominando-o-asp-net-mvc/ConhecimentosEssenciais/src/AppSemTemplate/Program.cs
@@ -1,100 +1,10 @@
-using AppSemTemplate.Data;
-using AppSemTemplate.Helper;
-using AppSemTemplate.Services;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using AppSemTemplate.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddControllersWithViews();
-
-builder.Services.AddControllersWithViews(options =>
-{
-    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-});
-
-// Adicionando suporte para mudan�a de conven��o de rota das areas
-//builder.Services.Configure<RazorViewEngineOptions>(options =>
-//{
-//    options.AreaPageViewLocationFormats.Clear();
-//    options.AreaPageViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
-//    options.AreaPageViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
-//    options.AreaPageViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
-//});
-
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-builder.Services.AddTransient<IOperacaoTransient, Operacao>();
-builder.Services.AddScoped<IOperacaoScoped, Operacao>();
-builder.Services.AddSingleton<IOperacaoSingleton, Operacao>();
-builder.Services.AddSingleton<IOperacaoSingletonInstance>(new Operacao(Guid.Empty));
-builder.Services.AddTransient<OperacaoService>();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = true;
-})
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<AppDbContext>();
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("PodeExcluirPermanentemente", policy =>
-        policy.RequireRole("Admin"));
-
-    options.AddPolicy("VerProdutos", policy =>
-        policy.RequireClaim("Produtos", "VI"));
-});
+builder.AddMvcConfiguration()
+        .AddIdentityConfiguration()
+        .AddDependencyInjectionConfiguration();
 
 var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-    app.UseHsts();
-
-// Redirecionamento para HTTPS
-app.UseHttpsRedirection();
-
-// Apenas para popular dados no banco para realizar testes
-app.UseDbMigrationHelper();
-
-// Configuracao que olha para a pasta "wwwroot" para usar os arquivos estaticos
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-app.UseAuthorization();
-
-// Forma de fazer um "transformador de rota" deixando a rota com um padrao especifico
-//builder.Services.AddRouting(options =>
-//    options.ConstraintMap["slugfy"] = typeof(RouteSlugfyParameterTransformer));
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller:slugfy=Home}/{action:slugfy=Index}/{id?}");
-
-app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
-
-using (var serviceScope = app.Services.CreateScope())
-{
-    var services = serviceScope.ServiceProvider;
-
-    var singService = services.GetRequiredService<IOperacaoSingleton>();
-
-    Console.WriteLine("Direto do Program.cs: " + singService.OperacaoId);
-}
-
+app.UseMvcConfiguration();
 app.Run();
