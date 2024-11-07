846549ae82885425ee579c64a44ea58e84ae009b
Daniel Corrêa <daniel.exatas@gmail.com>
2024-11-07 09:41:10 -0300

    Continuando com testes de unidade da aplicacao

--- 03-dominando-o-asp-net-mvc/ConhecimentosEssenciais/tests/Testes/ControllerTests.cs
+++ 03-dominando-o-asp-net-mvc/ConhecimentosEssenciais/tests/Testes/ControllerTests.cs
@@ -1,8 +1,10 @@
 using AppSemTemplate.Controllers;
 using AppSemTemplate.Data;
 using AppSemTemplate.Models;
+using AppSemTemplate.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using System.Security.Claims;
@@ -50,8 +52,11 @@ namespace Testes
             var mockHttpContext = new Mock<HttpContext>();
             mockHttpContext.Setup(c => c.User).Returns(principal);
 
+            // Imagem Service
+            var imagemService = new Mock<IImageUploadService>();
+
             // Controller
-            var controller = new ProdutosController(ctx)
+            var controller = new ProdutosController(ctx, imagemService.Object)
             {
                 ControllerContext = new ControllerContext
                 {
@@ -63,6 +68,82 @@ namespace Testes
             var result = controller.Index().Result;
 
             // ====> Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void ProdutoController_CriarProduto_Sucesso()
+        {
+            // ===> Arrange
+
+            // Resolvendo o AppDbContext
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            // Contexto
+            var ctx = new AppDbContext(options);
+
+            //IFormFile
+            var fileMock = new Mock<IFormFile>();
+            var fileName = "test.jpg";
+            fileMock.Setup(_ => _.FileName).Returns(fileName);
+
+            // Imagem Service
+            var imagemService = new Mock<IImageUploadService>();
+            imagemService.Setup(s => s.UploadArquivo(
+                new ModelStateDictionary(), fileMock.Object, It.IsAny<string>())
+            ).ReturnsAsync(true);
+
+            // Produto
+            var produto = new Produto()
+            {
+                Id = 1,
+                ImagemUpload = fileMock.Object,
+                Nome = "Teste",
+                Valor = 50
+            };
+
+            // Controller
+            var controller = new ProdutosController(ctx, imagemService.Object);
+
+            // ====> Act
+            var result = controller.CriarProduto(produto).Result;
+
+            // ====> Assert
+            Assert.IsType<RedirectToActionResult>(result);
+        }
+
+        [Fact]
+        public void ProdutoController_CriarProduto_ErroValidacaoProduto()
+        {
+            // ===> Arrange
+
+            // Resolvendo o AppDbContext
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            // Contexto
+            var ctx = new AppDbContext(options);
+
+            // Imagem Service
+            var imagemService = new Mock<IImageUploadService>();
+
+            // Controller
+            var controller = new ProdutosController(ctx, imagemService.Object);
+            controller.ModelState.AddModelError("Nome", "O campo nome é requerido");
+
+            // Produto
+            var produto = new Produto()
+            {
+            };
+
+            // ====> Act
+            var result = controller.CriarProduto(produto).Result;
+
+            // ====> Assert
+            Assert.False(controller.ModelState.IsValid);
             Assert.IsType<ViewResult>(result);
         }
     }
